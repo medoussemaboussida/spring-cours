a845f4a0cacc016e8311548de9afd9085ce24b6d
Boussida Mohamed Oussema <155064713+medoussemaboussida@users.noreply.github.com>
2024-11-19 20:36:49 +0100

    Delete tp2 directory

--- tp2/src/main/java/tn/esprit/tp2/entity/TypeChambre.java
+++
@@ -1,6 +0,0 @@
-package tn.esprit.tp2.entity;
-
-public enum TypeChambre {
-    SIMPLE, DOUBLE, TRIPLE;
-
-}
--- tp2/src/main/java/tn/esprit/tp2/repository/FoyerRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.tp2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tn.esprit.tp2.entity.Foyer;
-
-@Repository
-public interface FoyerRepository extends JpaRepository<Foyer, Long> {
-}
